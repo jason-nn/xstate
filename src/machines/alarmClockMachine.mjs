4e298fef7528022d0522f4f857b7d8d9d9a46f1e
Jason Ho <jason@swarm.work>
2022-01-02 01:03:35 +0800

    Add alarm clock machine

---
+++ src/machines/alarmClockMachine.mjs
@@ -0,0 +1,46 @@
+import { assign, createMachine, interpret } from 'xstate';
+
+const alarmClockMachine = createMachine(
+  {
+    id: 'alarmClock',
+    initial: 'idle',
+    states: {
+      idle: {
+        on: {
+          ALARM: { target: 'alarming' },
+        },
+      },
+      alarming: {
+        on: {
+          STOP: { target: 'idle' },
+        },
+        activities: ['beeping'],
+      },
+    },
+  },
+  {
+    activities: {
+      beeping: (context, event) => {
+        const beep = () => {
+          console.log('beep');
+        };
+
+        beep();
+
+        const beepInterval = setInterval(beep, 1000);
+
+        return () => clearInterval(beepInterval);
+      },
+    },
+  }
+);
+
+const alarmClockMachineService = interpret(alarmClockMachine);
+
+alarmClockMachineService.start();
+
+alarmClockMachineService.send({ type: 'ALARM' });
+
+setTimeout(() => {
+  alarmClockMachineService.send({ type: 'STOP' });
+}, 3000);
