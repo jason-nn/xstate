6ecadecafd8185861ba06a81ccddcd3753bc1bd8
Jason Ho <jason@swarm.work>
2022-01-02 01:14:03 +0800

    Add vending machine

---
+++ src/machines/vendingMachine.mjs
@@ -0,0 +1,32 @@
+import { assign, createMachine } from 'xstate';
+
+const vendingMachine = createMachine(
+  {
+    id: 'vendingMachine',
+    initial: 'idle',
+    context: {
+      deposited: 0,
+    },
+    states: {
+      idle: {
+        on: {
+          SELECT_ITEM: { target: 'vending', cond: 'sufficientDeposit' },
+          DEPOSIT_QUARTER: { actions: ['addQuarter'] },
+        },
+      },
+      vending: {},
+    },
+  },
+  {
+    actions: {
+      addQuarter: assign({
+        deposited: (context, event) => context.deposited + 25,
+      }),
+    },
+    guards: {
+      sufficientDeposit: (context, event) => context.deposited >= 100,
+    },
+  }
+);
+
+vendingMachine;
